332ebf02f408975401073bd129700740ddd752f6
developer3 <developer3@example.com>
2021-10-05 18:07:13 +0300

    Responsive (993px-769px) Complete!

--- Task 3/js/main.js
+++ Task 3/js/main.js
@@ -22,4 +22,17 @@ $(function(){
 	    }, 900);
 	    return false;
 	});
+
+	const menuBtn  = document.querySelector('.menu-btn');
+	let menuOpen = false;
+	menuBtn.addEventListener('click', () => {
+		if(!menuOpen) {
+			menuBtn.classList.add('open');
+			menuOpen = true;
+		}
+		else {
+			menuBtn.classList.remove('open');
+			menuOpen = false;
+		}
+	});
 });
