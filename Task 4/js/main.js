d2a41f1fca2d379f2690dc09d7f0de09d2bce781
Schattenoff <schattenoff@gmail.com>
2021-10-07 23:17:00 +0300

    Task 4 Complete!

---
+++ Task 4/js/main.js
@@ -0,0 +1,17 @@
+var second = 0;
+var minute = 0;
+var hour = 0;
+
+var day = new Date();
+
+setInterval(
+    function() {
+        day = new Date();
+        second = day.getSeconds() * 6;
+        minute = day.getMinutes() * 6;
+        hour = day.getHours() * 30 + Math.round(minute / 12);
+        document.getElementById("second-hand").style.transform = "rotate(" + second + "deg)";
+        document.getElementById("minute-hand").style.transform = "rotate(" + minute + "deg)";
+        document.getElementById("hour-hand").style.transform = "rotate(" + hour + "deg)";
+    },1000
+);
