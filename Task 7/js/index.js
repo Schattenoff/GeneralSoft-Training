f9b9cea9e806387d575f947a72626e550ad187b1
developer3 <developer3@example.com>
2021-10-15 15:57:29 +0300

    Task 7 process

---
+++ Task 7/js/index.js
@@ -0,0 +1,10 @@
+function totalSquare(array) {
+    
+}
+
+const square = new Square(5);
+const rectangle = new Rectangle(3, 6);
+const circle = new Circle(4);
+const arrayOfFigures = [square, rectangle, circle];
+
+console.log(totalSquare(arrayOfFigures))
