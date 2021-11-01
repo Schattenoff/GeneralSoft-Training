0933b88124e629606e6576ce40b0b01a61405eae
Schattenoff <schattenoff@gmail.com>
2021-11-02 00:33:59 +0300

    id, username

--- Task 8 (Final)/js/calendar.js
+++ Task 8 (Final)/js/calendar.js
@@ -8,7 +8,7 @@ const dt = new Date();
 
 const renderCalendar = () => {
     select.selectedIndex = dt.getMonth();
-    
+  
     dt.setDate(1);
     
     const year = dt.getFullYear();
@@ -19,30 +19,36 @@ const renderCalendar = () => {
     const firstDayIndex = dt.getDay();
     const lastDayIndex = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDay();
     const nextDays = 7 - lastDayIndex;
-    
     let displayYear = document.querySelector('.year');
     displayYear.innerHTML = year;
     
     let days = "";
-    
-    for (let x = firstDayIndex-1; x > 0; x--) {
+
+    let dayIndex = 0;
+    for (let t = 0; t <= firstDayIndex; t++) {
+        if (firstDayIndex == 0) {
+            dayIndex = 7;
+        }
+        else dayIndex = firstDayIndex;
+    }
+    for (let x = dayIndex-1; x > 0; x--) {
         days += `<div class="calendar-box prevDate">${prevLastDay - x +1}</div>`;
     }
     
-    for(let i = 1; i <= lastDay; i++) {
-        let day = document.querySelector('.calendar-box');
+    for (let i = 1; i <= lastDay; i++) {
+        let day = document.getElementsByClassName('calendar-box');
         if (i === new Date().getDate() && dt.getMonth() === new Date().getMonth() && dt.getFullYear() === new Date().getFullYear()) {
             days += `<div class="calendar-box today">${i}</div>`
         } else {
             days += `<div class="calendar-box">${i}</div>`;
+            day.textContent = 2;
         } 
     }
     
-    for(let j = 1; j <= nextDays; j++) {
+    for (let j = 1; j <= nextDays; j++) {
         days += `<div class="calendar-box nextDate">${j}</div>`;
         monthDays.innerHTML = days;
     }
-    console.log(dt);
 }
 
 function change() {
@@ -67,6 +73,12 @@ helper.disableBack();
 function logout() {
     helper.message(true, "Ok! Logout!");
 }
+
+
+let name = document.querySelector('.name');
+let users = JSON.parse(localStorage.getItem('users'));
+let id = JSON.parse(localStorage.getItem('id'));
+name.textContent = `Name: ${users[id].userName}`
 
 
 
