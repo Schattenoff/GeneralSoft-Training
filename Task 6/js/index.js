a979ca334e08e0177ab9f9b101c94f1ab47ea509
developer3 <developer3@example.com>
2021-11-10 17:09:13 +0300

    Array Prototype

--- Task 6/js/index.js
+++ Task 6/js/index.js
@@ -1,23 +1,56 @@
 function MyArray(array) {
-    return {
-        first: function() {
-            return  array[0]
-        },
-        last: function() {
-            return array[array.length-1]
-        },
-        skip: function(num) {
-            return array.slice(2, num);
-        },
-        take: function(num) {
-            return array.slice(0, num);
-        },
+    this.array = array;
+}
+
+MyArray.prototype.first = function() {
+    return this.array[0];
+}
+
+MyArray.prototype.last = function() {
+    return this.array[this.array.length-1];
+}
+
+MyArray.prototype.skip = function(index) {
+    let result = [];
+    for(let i = 0; i < this.array.length; i++) {
+        if (i >= index) {
+            result[i - index] = this.array[i];
+        }
     }
+    return new MyArray(result);
+}
+
+
+MyArray.prototype.take = function(index) {
+    let result = [];
+    for(let i = 0; i < index; i++) {
+        result[i] = this.array[i];
+    }
+    return new MyArray(result);
+}
+
+MyArray.prototype.map = function(fn) {
+    let result = [];
+    result = this.array.map(fn);
+    return result;
+}
+
+MyArray.prototype.filter = function(fn) {
+    let result = [];
+    result = this.array.filter(fn);
+    return result;
 }
 
 var arr = new MyArray([3, 5, 2, 4, 1]);
-
 console.log(arr.first());
 console.log(arr.last());
-console.log(arr.skip(5));
-console.log(arr.take(2));
+console.log(arr.skip(2).array);
+console.log(arr.take(2).array);
+console.log(arr.take(4).skip(3));
+console.log(arr.take(4).skip(2).first());
+console.log(arr.map(function(item) {
+    return item * item;
+}));
+console.log(arr.filter(function(item) {
+    return item % 2;
+}))
