623ca8c7b2e8843aead8aee7db9fe7d4be2fd1c1
Schattenoff <schattenoff@gmail.com>
2021-11-18 22:05:00 +0300

    refactoring method reduce

--- Task 7/js/index.js
+++ Task 7/js/index.js
@@ -26,14 +26,14 @@ class Circle {
 }
 
 function totalSquare(array) {
-    return array.reduce(function(item) {
-        return console.log(item.square());
-    
-}
-    
+    return array.reduce(function (total, currentItem) {
+    total += currentItem.square();
+    return total;
+  }, 0);
+}    
+
 const square = new Square(5);
 const rectangle = new Rectangle(3, 6);
 const circle = new Circle(4);
 const arrayOfFigures = [square, rectangle, circle];
-console.log(square.square());
 console.log(totalSquare(arrayOfFigures));
